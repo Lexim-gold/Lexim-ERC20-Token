00bc3d87bafb6549b99043b6f016b87760daf286
opcheese <opcheese>
2023-03-03 15:55:13 +0500

    initial code commit

---
+++ README.md
@@ -0,0 +1,83 @@
+# Lexim ERC20 Token 
+Lexim-issued ERC20 token backed by physical gold smart contract repository.
+
+https://lexim.gold
+
+## Contract Specification
+
+Lexim-issued ERC20 is an ERC20 token that is Centrally Minted and Burned by Lexim,
+representing the physical ownership of gold bars.
+
+### ERC20 Token
+
+The public interface of Lexim-issued ERC20 is the ERC20 interface
+specified by [EIP-20](https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md).
+
+- `name()`
+- `symbol()`
+- `decimals()`
+- `totalSupply()`
+- `balanceOf(address who)`
+- `transfer(address to, uint256 value)`
+- `approve(address spender, uint256 value)`
+- `allowance(address owner, address spender)`
+- `transferFrom(address from, address to, uint256 value)`
+
+And the usual events.
+
+- `event Transfer(address indexed from, address indexed to, uint256 value)`
+- `event Approval(address indexed owner, address indexed spender, uint256 value)`
+
+Typical interaction with the contract will use `transfer` to move the token as payment.
+Additionally, a pattern involving `approve` and `transferFrom` can be used to allow another 
+address to move tokens from your address to a third party without the need for the middleperson 
+to custody the tokens, such as in the 0x protocol. 
+
+### Controlling the token supply
+
+The total supply of Lexim-issued ERC20 is backed by physical gold under the custody of Lexim.
+To make gold bar management more transparent we support a list of gold bar ids - unique gold bar serial numbers - that are used as backing for ERC20. The list is stored on the blockchain an available via the smart contract. Specifically via smart contract method
+
+- `getGoldBars`
+
+Gold bar id list change Events:
+- `event GoldBarIdAdded(string indexed goldBarId)`
+- `event GoldBarIdRemoved(string indexed goldBarId)`
+
+### Pausing the contract
+
+In the event of a critical security threat, Lexim has the ability to pause transfers of the Lexim token. The ability to pause is controlled by an address with a PAUSER_ROLE.
+
+The simple model for pausing transfers following OpenZeppelin's
+[Pausable](https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#ERC20Pausable).
+
+### Fees
+
+Lexim charges a set fee rate for all on-chain transfers of Lexim-issued ERC20 in order to offset storage fees of gold bars in our vaults.
+The fee controller has the ability to set the fee accumulator and the fee rate (measured in 1/100th of a basis point).
+Lexim will never change the fee rate without prior notice as we take transparency very seriously.
+
+#### Fee Rounding
+The `transfer` function takes the debit amount as input, and computes the fee and credit to the recipient as
+```
+fee = debit.mul(feeRate).div(feeParts)
+credit = debit.sub(fee)
+```
+Note that div truncates to an integer (and therefore 18 decimal effective precision).
+
+### Asset Controller Role
+
+As required by our regulators, we have introduced a role for asset protection to freeze or seize the assets of a criminal party when required to do so by law, including by court order or other legal process.
+
+The `CONTROLLER_ROLE` can freeze and unfreeze the Lexim-issued ERC20 balance of any address on chain.
+
+Freezing is something that Lexim will not do on its own accord, and as such we expect to happen extremely rarely.
+
+### Permits
+
+In order to allow for gas-less transactions we have implemented support for EIP-2612 based on https://docs.openzeppelin.com/contracts/4.x/api/token/erc20#ERC20Permit.
+
+### Upgradeability
+
+We believe that smart contract should not be changeable on the whim of anyone including the developers. Consequently our ERC20 smart contract is NOT upgradable.
+
---
+++ contracts/KrypzToken.sol
@@ -0,0 +1,292 @@
+// SPDX-License-Identifier: Copyright all rights reserved.
+pragma solidity ^0.8.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+
+/// @title A ERC20 token 
+/// @dev Supports transfer fees
+contract KrypzToken is ERC20, ERC20Burnable, Pausable, AccessControl, ERC20Permit {
+    
+    /// @dev Constant for the pauser role
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    /// @dev Constant for the minter role
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    /// @dev Constant for the controller role
+    bytes32 public constant CONTROLLER_ROLE = keccak256("CONTROLLER_ROLE");
+    /// @dev Constant for the fee controller role
+    bytes32 public constant FEE_ROLE = keccak256("FEE_ROLE");
+
+    // Token name
+    string private _changeableName;
+
+    // Token symbol
+    string private _changeableSymbol;
+
+    // Token symbol
+    /// @dev Name of the token when deployed
+    string public originalName;
+
+    mapping(address => bool) internal _frozen;    
+
+    /// @dev Current fee rate
+    uint256 public feeRate;
+    /// @dev Address where fees get sent
+    address public feeAccumulator;
+
+    /// @dev Stuct to support dynamic list of goldbar ids
+    struct Set {
+        string[] values;
+        mapping (string => uint) isIn;
+    }
+
+    Set internal _goldBars; 
+    
+    /// @dev Fee rate denominator
+    uint256 public constant feeParts = 1000000;
+    /**
+     * @dev Emitted when `addr` has been frozen
+     */
+    event AddressFrozen(address indexed addr);
+    /**
+     * @dev Emitted when `addr` has been unfrozen
+     */
+    event AddressUnfrozen(address indexed addr);
+    /**
+     * @dev Emitted in addition to transfer event during transfer when fee is being transfered to accumulator
+     */
+    event FeeCollected(address indexed from, address indexed to, uint256 value);
+    /**
+     * @dev Emitted every time trasfer fee rate is changed
+     */
+    event FeeRateSet(
+        uint256 indexed oldFeeRate,
+        uint256 indexed newFeeRate
+    );
+
+    /**
+     * @dev Emitted every time trasfer fee accumulator is changed
+     */
+    event FeeAccumulatorSet(
+        address indexed oldFeeAccumulator,
+        address indexed newFeeAccumulator
+    );
+
+     /**
+     * @dev Gold bar id added
+     */
+    event GoldBarIdAdded(
+        string indexed goldBarId
+    );
+
+    /**
+    * @dev Gold bar id removce
+    */
+    event GoldBarIdRemoved(
+        string indexed goldBarId
+    );
+
+
+
+    constructor(string memory initialName, string memory initialSymbol) ERC20(initialName, initialSymbol)
+     ERC20Permit(initialName) {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(CONTROLLER_ROLE, msg.sender);
+        _grantRole(FEE_ROLE, msg.sender);
+        _changeableName = initialName;
+        _changeableSymbol = initialSymbol;
+        originalName = initialName;
+        feeRate = 0;
+        feeAccumulator = msg.sender;
+    }
+    /**
+     * @dev See {IERC721Metadata-name}.
+     */
+    function name() public view  override returns (string memory) {
+        return _changeableName;
+    }
+
+    /**
+     * @dev See {IERC721Metadata-symbol}.
+     */
+    function symbol() public view  override returns (string memory) {
+        return _changeableSymbol;
+    }
+    /**
+     * @dev set name and symbol
+     */
+    function setNameAndSymbol(string memory newName, string memory newSymbol)
+     public  onlyRole(CONTROLLER_ROLE) {
+        _changeableName = newName;
+        _changeableSymbol = newSymbol;
+    }
+
+    /**
+     * @dev returns list of goldbars. Order is NOT guaranteed to be constant.
+     */
+    function getGoldBars() public view returns(string[] memory) {
+        return _goldBars.values;
+    }
+
+     /**
+     * @dev Move the last goldbar id to the deleted spot.
+     * Remove the last element.
+    */
+    function removeGoldBar(string calldata goldBarId) public  onlyRole(CONTROLLER_ROLE) {       
+        uint pos = _goldBars.isIn[goldBarId];
+        require(pos > 0, "Gold bar not found");
+        string memory lastVal = _goldBars.values[_goldBars.values.length-1];
+        _goldBars.values[pos-1] = _goldBars.values[_goldBars.values.length-1];
+        _goldBars.values.pop();
+        _goldBars.isIn[lastVal] = pos;
+        emit GoldBarIdRemoved(goldBarId);
+    }
+
+    /**
+     * @dev Add gold bar id to the list
+    */
+    function addGoldBar(string calldata goldbarId) public  onlyRole(CONTROLLER_ROLE) {
+        require(_goldBars.isIn[goldbarId] == 0, "Gold bar already exists");
+        _goldBars.values.push(goldbarId);
+        _goldBars.isIn[goldbarId] = _goldBars.values.length;
+        emit GoldBarIdAdded(goldbarId);
+    }
+
+    /**
+     * @dev Add list of gold bar ids to the list
+    */
+    function addGoldBars(string[] calldata goldbarIds) public onlyRole(CONTROLLER_ROLE)  {
+        for (uint256 i=0;i<goldbarIds.length;i++) {
+            addGoldBar(goldbarIds[i]);
+        }
+    }
+
+    /**
+     * @dev Remove list of gold bar ids from the list
+    */
+     function removeGoldBars(string[] calldata goldbarIds) public onlyRole(CONTROLLER_ROLE)  {
+        for (uint256 i=0;i<goldbarIds.length;i++) {
+            removeGoldBar(goldbarIds[i]);
+        }
+    }
+
+
+    /**
+     * @dev Freezes an address balance from being transferred.
+     * @param addr The address to freeze.
+     */
+    function freeze(address addr) public onlyRole(CONTROLLER_ROLE) {
+        require(!_frozen[addr], "address already frozen");
+        _frozen[addr] = true;
+        emit AddressFrozen(addr);
+    }
+
+    /**
+     * @dev Unfreezes an address balance allowing transfer.
+     * @param addr The new address to unfreeze.
+     */
+    function unfreeze(address addr) public onlyRole(CONTROLLER_ROLE) {
+        require(_frozen[addr], "address already unfrozen");
+        _frozen[addr] = false;
+        emit AddressUnfrozen(addr);
+    }
+
+    /**
+    * @dev Gets whether the address is currently frozen.
+    * @param addr The address to check if frozen.
+    * @return A bool representing whether the given address is frozen.
+    */
+    function isFrozen(address addr) public view returns (bool) {
+        return _frozen[addr];
+    }
+
+     /**
+     * Pause the contract preventing transfers
+     */
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    /**
+     * Unpause the contract
+     */
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function mint(address to, uint256 amount) public  onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount)
+        internal
+        whenNotPaused
+        override
+    {
+        super._beforeTokenTransfer(from, to, amount);
+        require(!_frozen[to] && !_frozen[from], "address frozen");
+    }
+
+    /**
+     * @dev Sets a new fee recipient address.
+     * @param newFeeAccumulator The address allowed to collect transfer fees for transfers.
+     */
+    function setFeeAccumulator(address newFeeAccumulator) public onlyRole(FEE_ROLE) {
+        require(newFeeAccumulator != address(0), "cannot set fee accumulator to null");
+        address oldFeeAccumulator = feeAccumulator;
+        feeAccumulator = newFeeAccumulator;
+        emit FeeAccumulatorSet(oldFeeAccumulator, newFeeAccumulator);
+    }
+
+    /**
+     * @dev Sets a new fee rate.
+     * @param _newFeeRate The new fee rate to collect as transfer fees for transfers.
+     */
+    function setFeeRate(uint256 _newFeeRate) public onlyRole(FEE_ROLE) {
+        require(_newFeeRate <= feeParts, "cannot set fee rate above 100%");
+        uint256 _oldFeeRate = feeRate;
+        feeRate = _newFeeRate;
+        emit FeeRateSet(_oldFeeRate, feeRate);
+    }
+
+    /**
+     * @dev Moves `amount` of tokens from `sender` to `recipient` subtracting fee
+     * fee is transfered to feeAccumulator
+     *
+     * Emits 2 {Transfer} events. Emits 2 {FeeCollected}  event
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual override {    
+        uint256 feeAmount = getFeeFor(amount);
+       
+        uint256 newAmount = amount - feeAmount;
+        if (feeAmount>0){
+            emit FeeCollected(sender, feeAccumulator, feeAmount);
+            super._transfer(sender, feeAccumulator, feeAmount);
+        }
+        super._transfer(sender, recipient, newAmount);
+    }
+
+    /**
+    * @dev Gets a fee for a given value
+    * ex: given feeRate = 200 and feeParts = 1,000,000 then getFeeFor(10000) = 2
+    * @param value The amount to get the fee for.
+    */
+    function getFeeFor(uint256 value) public view returns (uint256) {
+        if (feeRate == 0) {
+            return 0;
+        }
+        return value*feeRate/feeParts;
+    }
+    
+}
